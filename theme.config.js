45d9460a1a154d06ce99bdb8867a1813a8a451f6
Aliif Arief <aliif.arief.m@gmail.com>
2022-01-04 20:58:53 +0700

    config

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/aliifam/dok',
+  docsRepositoryBase: 'https://github.com/aliifam/dok',
+  titleSuffix: ' – Dok',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Dok</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Diary
       </span>
     </>
   ),
@@ -15,16 +15,16 @@ export default {
       <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta httpEquiv="Content-Language" content="id" />
+      <meta name="description" content="Dok adalah sebuah diary dokumentasi." />
+      <meta name="og:description" content="Dok adalah sebuah diary dokumentasi.." />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
+      <meta name="twitter:image" content="https://cdn.statically.io/og/theme=dark/Dok%20Diary.jpg" />
+      <meta name="twitter:site:domain" content="dok.aliif.space" />
+      <meta name="twitter:url" content="https://dok.aliif.space" />
+      <meta name="og:title" content="Dok Diary" />
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="apple-mobile-web-app-title" content="Dok" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -60,8 +60,9 @@ export default {
   search: true,
   prevLinks: true,
   nextLinks: true,
+  defaultMenuCollapsed: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>MIT {new Date().getFullYear()} © Dok Diary.</>,
   unstable_faviconGlyph: '👋',
 }
